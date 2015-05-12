1e9e52b9c31421ee89fb15394f45c5a3db9e2f13
sarwanpasha <sarwanpasha@gmail.com>
2015-05-12 20:53:30 +0500

    Sending Email

---
+++ app/src/main/java/com/example/pasha/sendingemail/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.pasha.sendingemail;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        String[] to =
+                {"sarwanpasha@gmail.com",
+                        "pashapap@yahoo.com"};
+        String[] cc = {"pashapap@yahoo.com"};
+        sendEmail(to, cc, "Hello", "Hello my friends!");
+        Toast.makeText(getBaseContext(), "Email Sended", Toast.LENGTH_LONG).show();
+    }
+
+    private void sendEmail(String[] emailAddresses, String[] carbonCopies,
+                           String subject, String message)
+    {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        String[] to = emailAddresses;
+        String[] cc = carbonCopies;
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
+        emailIntent.putExtra(Intent.EXTRA_CC, cc);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+        emailIntent.setType("message/rfc822");
+        startActivity(Intent.createChooser(emailIntent, "Email"));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
